23fc8bfd47983488e86db00d859d4cfd5329fa0f
LP <44683161+last-person@users.noreply.github.com>
2019-07-03 17:47:03 -0500

    Added intial I2C LCD stuff

--- webserver/webserver.ino
+++ webserver/webserver.ino
@@ -1,17 +1,19 @@
-
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h> 
+#include <LiquidCrystal_I2C.h>
+
 
 #include "index.h" //Our HTML webpage contents
 
 //SSID and Password for ESP8266
-const char* ssid = "Power Meter";
+const char* ssid = "PowerMeter";
 const char* password = "" ;
 const String s = MAIN_page; //Read HTML page
 
 ESP8266WebServer server(80); //Server on port 80
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
 
 //===============================================================
 // This routine is executed when you open its IP in browser
@@ -30,6 +32,11 @@ void handlePostForm(){
   String userInput = server.arg("userInput");
   Serial.println("Text received. Contents: ");
   Serial.println(userInput); 
+  lcd.clear();
+  lcd.setCursor(0,0);
+  //lcd.print("Text received. Contents: ");
+  //lcd.setCursor(0,1);
+  lcd.print(userInput); 
   server.send(200, "text/html", s);
 }
 void handleNotFound() {
@@ -41,7 +48,7 @@ void handleNotFound() {
 void setup(void){
   Serial.begin(9600);
   Serial.println("Setting UP...");
-  WiFi.mode(WIFI_AP);           //Only Access point
+  //WiFi.mode(WIFI_AP);           //Only Access point
   WiFi.softAP(ssid, password);  //Start HOTspot removing password will disable security
  
   IPAddress myIP = WiFi.softAPIP(); //Get IP address
@@ -60,10 +67,19 @@ void setup(void){
 
   server.begin();                  //Start server
   Serial.println("HTTP server started");
-}
+
+
+//============= set up LCD ====================================
+  lcd.begin(16,2);
+  lcd.init();
+  lcd.setCursor(0,0);
+  lcd.backlight();
+}  
+
 //==============================================================
 //                     LOOP
 //==============================================================
 void loop(void){
   server.handleClient();          //Handle client requests
+ 
 }
